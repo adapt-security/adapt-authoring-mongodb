0623b50bf94a7014180b432cc6732db8a7e2efcb
Thomas Taylor <thomas.taylor@kineo.com>
2019-01-21 15:53:08 +0000

    Change file name

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const mongodb = require('./lib/mongodb');
-module.exports = mongodb;
+const MongooseSchema = require('./lib/mongooseschema');
+
+module.exports = {
+  Module: mongodb,
+  MongooseSchema: MongooseSchema
+};
