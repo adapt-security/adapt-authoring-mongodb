ed1bbc2d891d2984053c017babcca9b683ccce87
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-10 18:41:04 +0100

    Add config schema

---
+++ conf/config.schema.js
@@ -0,0 +1,16 @@
+module.exports = {
+  definition: {
+    host: {
+      type: 'String',
+      required: true
+    },
+    port: {
+      type: 'Number',
+      required: true
+    },
+    dbname: {
+      type: "String",
+      required: true
+    }
+  }
+};
