592851080893e6528e56d55c5835fec3971098ac
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-19 11:59:50 +0100

    Add descriptions to config

--- conf/config.schema.js
+++ conf/config.schema.js
@@ -2,15 +2,18 @@ module.exports = {
   definition: {
     host: {
       type: 'String',
-      required: true
+      required: true,
+      description: 'Name of the host machine where the Mongo database is running'
     },
     port: {
       type: 'Number',
-      required: true
+      required: true,
+      description: 'Port where the Mongo database is listening'
     },
     dbname: {
       type: "String",
-      required: true
+      required: true,
+      description: 'Name of the Mongo database to connect to'
     }
   }
 };
