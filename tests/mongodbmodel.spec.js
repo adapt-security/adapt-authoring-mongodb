405111537aff4deb475e002ca117c934b38f3310
Thomas Taylor <thomas.taylor@kineo.com>
2019-11-01 22:48:57 +0000

    Add skeleton test

---
+++ tests/mongodbmodel.spec.js
@@ -0,0 +1,28 @@
+const should = require('should');
+
+describe('MongoDBModel', function() {
+  describe('#SchemaTypes()', function() {
+    it('should return an object', function() {
+      false.should.be.true();
+    });
+  });
+  describe('#constructor()', function() {
+    it('should expose Mongoose.Schema#plugin()', function() {
+      false.should.be.true();
+    });
+    it('should expose Mongoose.Schema#.add()', function() {
+      false.should.be.true();
+    });
+    it('should expose Mongoose.Schema#pre()', function() {
+      false.should.be.true();
+    });
+    it('should expose Mongoose.Schema#post()', function() {
+      false.should.be.true();
+    });
+  });
+  describe('#compile()', function() {
+    it('should cache the compiled model', function() {
+      false.should.be.true();
+    });
+  });
+});
