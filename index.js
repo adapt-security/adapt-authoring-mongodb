54d98aa27b422a7b26d6ec0e32d3fb51d07855ea
Chris Steele <chris.steele@kineo.com>
2024-01-24 09:08:54 +0000

    Corrected path

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  * MongoDB integration
  * @namespace mongodb
  */
- export { default as ObjectIdUtils } from './lib/ObjectIdUtils'
+ export { default as ObjectIdUtils } from './lib/ObjectIdUtils.js'
  export { default } from './lib/MongoDBModule.js'
  
