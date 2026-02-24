cd01f6b0858bc5d367ec460dcaa9e9a82d90934b
Tom Taylor <thomas.taylor@kineo.com>
2026-02-24 12:32:04 +0000

    Breaking: Rename utils for clarity and inline ObjectIdUtils (fixes #50) (#51)

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
  * MongoDB integration
  * @namespace mongodb
  */
-export { default as ObjectIdUtils } from './lib/ObjectIdUtils.js'
-export { create, isObjectId, isValid, parse, parseIds } from './lib/utils.js'
+export { convertObjectIds, createObjectId, isObjectId, isValidObjectId, parseObjectId } from './lib/utils.js'
 export { default } from './lib/MongoDBModule.js'
--- tests/utils-createObjectId.spec.js
+++ tests/utils-createObjectId.spec.js
@@ -1,22 +1,22 @@
 import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
 import { ObjectId } from 'mongodb'
-import { create } from '../lib/utils/create.js'
+import { createObjectId } from '../lib/utils/createObjectId.js'
 
-describe('create()', () => {
+describe('createObjectId()', () => {
   it('should return an ObjectId instance', () => {
-    const id = create()
+    const id = createObjectId()
     assert.ok(id instanceof ObjectId)
   })
 
   it('should create unique IDs each time', () => {
-    const id1 = create()
-    const id2 = create()
+    const id1 = createObjectId()
+    const id2 = createObjectId()
     assert.notEqual(id1.toString(), id2.toString())
   })
 
   it('should return a 24-character hex string representation', () => {
-    const id = create()
+    const id = createObjectId()
     assert.match(id.toString(), /^[0-9a-f]{24}$/)
   })
 })
